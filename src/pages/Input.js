93791482f956a844c5e0d32b63145b547ce2e590
Matija0 <90220699+Matija0@users.noreply.github.com>
2023-02-11 16:37:17 +0100

    Update

--- src/pages/Input.js
+++ src/pages/Input.js
@@ -6,86 +6,61 @@ import Main from './Main';
 
 
 
-export default function Input(){
-    const [message, setMessage] = useState('');
-    const handleChange = event => {
-    setMessage(event.target.value);};
-
-    const [message2, setMessage2]= useState('');
-    const handleChange2=event=>{
-        setMessage2(event.target.value)
-    }
-    const [message3, setMessage3]= useState('');
-    const handleChange3=event=>{
-        setMessage3(event.target.value)
-    }
-    const [message4, setMessage4]= useState('');
-    const handleChange4=event=>{
-        setMessage4(event.target.value)
-    }
-    const [message5, setMessage5]= useState('');
-    const handleChange5=event=>{
-        setMessage5(event.target.value)
-    }
-    const [message6, setMessage6]= useState('');
-    const handleChange6=event=>{
-        setMessage6(event.target.value)
-    }
-    const [message7, setMessage7]= useState('');
-    const handleChange7=event=>{
-        setMessage7(event.target.value)
-    }
-    
+export default function Input({setData,post}){
+    const [title, setTitle] = useState('');
+    const [location, setLocation] = useState('');
+    const [googleMapsUrl, setUrl] = useState('');
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+    const [description, setDescription] = useState('');
+    const [imageUrl, setImageUrl] = useState('');
+    const Save=(e)=>{
+        e.preventDefault()
+        const obj={
+            title: title,
+            location: location,
+            googleMapsUrl: googleMapsUrl,
+            startDate: startDate,
+            endDate: endDate,
+            description: description,
+            imageUrl: imageUrl
+        }
+        setData(obj)
+       
+  post()
+        }
     
     return(
         <>
-        <form className="form">
+        <form onSubmit={Save} className="form w-100">
        
-            <input type="text" placeholder=" Enter title"     onChange={handleChange} value={message}  />
+            <input type="text" placeholder=" Enter title"     onChange={(e)=>setTitle(e.target.value)} value={title}  />
         
        
-            <input type="text" placeholder=" Enter location"  onChange={handleChange2} value={message2}/>
+            <input type="text" placeholder=" Enter location"  onChange={(e)=>setLocation(e.target.value)} value={location}/>
         
         
-            <input type="text" placeholder="Enter google maps link" onChange={handleChange3} value={message3} />
+            <input type="text" placeholder="Enter google maps link" onChange={(e)=>setUrl(e.target.value)} value={googleMapsUrl} />
         
         
-            <input type="text" placeholder="Enter date of start" onChange={handleChange4} value={message4} />
+            <input type="text" placeholder="Enter date of start" onChange={(e)=>setStartDate(e.target.value)} value={startDate} />
         
         
-            <input type="text" placeholder="Enter date of end" onChange={handleChange5} value={message5} />
+            <input type="text" placeholder="Enter date of end" onChange={(e)=>setEndDate(e.target.value)} value={endDate} />
     
         
-            <input type="text"  placeholder="Enter description" onChange={handleChange6} value={message6}/>
+            <input type="text"  placeholder="Enter description" onChange={(e)=>setDescription(e.target.value)} value={description}/>
         
         
-            <input type="text" placeholder="Enter image link" onChange={handleChange7} value={message7}/>
+            <input type="text" placeholder="Enter image link" onChange={(e)=>setImageUrl(e.target.value)} value={imageUrl}/>
 
-            <button className="btn" onClick={Save}>Save</button>
+            <button className="btn" type='submit'>Save</button>
         </form>
         
         
         </>
     )
-    function Save(){
-        const obj={
-            title: {message},
-            location: {message2},
-            googleMapsUrl: {message3},
-            startDate: {message4},
-            endDate: {message5},
-            description: {message6},
-            imageUrl: {message7}
-        }
-    
-    return(
-       <></>
-       
-    )
-     
 
-
-    }
 }
 
 
